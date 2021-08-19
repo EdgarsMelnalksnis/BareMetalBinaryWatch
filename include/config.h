b2dc2ad250e5a4dd9b4bc9aca0611b35584f45af
EdgarsMelnalksnis <edgars.melnalksnis@gmail.com>
2021-08-19 12:08:02 +0300

    added function to blink LED, .bin files to .gitignore

--- include/config.h
+++ include/config.h
@@ -6,6 +6,14 @@
 #define GPIO_7_MODER_POS_1 (15u)
 #define GPIO_8_MODER_POS_0 (16u)
 #define GPIO_8_MODER_POS_1 (17u)
+#define GPIO_9_MODER_POS_0 (18u)
+#define GPIO_9_MODER_POS_1 (19u)
+#define GPIO_10_MODER_POS_0 (20u)
+#define GPIO_10_MODER_POS_1 (21u)
+#define GPIO_11_MODER_POS_0 (22u)
+#define GPIO_11_MODER_POS_1 (23u)
+#define GPIO_12_MODER_POS_0 (24u)
+#define GPIO_12_MODER_POS_1 (25u)
 
 #ifdef __cplusplus
 #define   __I     volatile             /*!< Defines 'read only' permissions */
@@ -35,7 +43,10 @@
 
 #define LED_PIN_7    (7u)
 #define LED_PIN_8    (8u)
-
+#define LED_PIN_9    (9u)
+#define LED_PIN_10   (10u)
+#define LED_PIN_11   (11u)
+#define LED_PIN_12   (12u)
 
 typedef struct
 {
--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include "config.h"
 
+void blink_led(uint8_t gpio_anode,uint8_t gpio_cathode);
 
 int main(void)
 {
@@ -7,18 +8,44 @@ int main(void)
     RCC->IOPENR   |= RCC_IOPENR_GPIOAEN;
 
     /*Configure GPIO pin : (OUTPUT is 0x01)*/
-    GPIOA->MODER |= ((1<<GPIO_7_MODER_POS_0)|(1<<GPIO_8_MODER_POS_0));//sets 14 and 16 bit to 1
-    GPIOA->MODER &= ~((1<<GPIO_7_MODER_POS_1)|(1<<GPIO_8_MODER_POS_1));//sets 15 adn 17 bit to 0
 
-    GPIOA->ODR |= (1 << LED_PIN_7);
-    GPIOA->ODR &= ~(1 << LED_PIN_8);
 
-    while (1)
+    while(1)
     {
+        blink_led(LED_PIN_7,LED_PIN_8);
+        blink_led(LED_PIN_7,LED_PIN_9);
+        blink_led(LED_PIN_7,LED_PIN_10);
+        blink_led(LED_PIN_7,LED_PIN_11);
+        blink_led(LED_PIN_7,LED_PIN_12);
+        blink_led(LED_PIN_8,LED_PIN_9);
+        blink_led(LED_PIN_8,LED_PIN_10);
+        blink_led(LED_PIN_8,LED_PIN_11);
+        blink_led(LED_PIN_8,LED_PIN_12);
+        blink_led(LED_PIN_9,LED_PIN_10);
+        blink_led(LED_PIN_9,LED_PIN_11);
+        blink_led(LED_PIN_9,LED_PIN_12);
+        blink_led(LED_PIN_10,LED_PIN_11);
+        blink_led(LED_PIN_10,LED_PIN_12);
+        blink_led(LED_PIN_11,LED_PIN_12);
+        blink_led(LED_PIN_8,LED_PIN_7);
+        blink_led(LED_PIN_9,LED_PIN_7);
+        blink_led(LED_PIN_10,LED_PIN_7);
+        blink_led(LED_PIN_9,LED_PIN_8);
+        blink_led(LED_PIN_12,LED_PIN_7);
+        blink_led(LED_PIN_11,LED_PIN_7);
 
-        GPIOA->ODR ^= (1 << LED_PIN_7);
-        GPIOA->ODR ^= (1 << LED_PIN_8);
     }
+
+}
+
+void blink_led(uint8_t gpio_anode,uint8_t gpio_cathode)
+{
+    GPIOA->MODER |= ((1<<(gpio_anode*2))|(1<<(gpio_cathode*2)));//sets 14 and 16 bit to 1
+    GPIOA->MODER &= ~((1<<(gpio_anode*2+1))|(1<<(gpio_cathode*2+1)));//sets 15 adn 17 bit to 0
+    GPIOA->ODR |= (1 << gpio_anode);
+    GPIOA->ODR &= ~(1 << gpio_cathode);
+    GPIOA->MODER |= ((1<<(gpio_anode*2))|(1<<(gpio_cathode*2)));//sets 14 and 16 bit to 1
+    GPIOA->MODER |=((1<<(gpio_anode*2+1))|(1<<(gpio_cathode*2+1)));
 
 }
 
