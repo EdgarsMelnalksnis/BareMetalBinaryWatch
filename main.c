b60770091bbd67eb63ee599ce47bccf3a8eca142
EdgarsMelnalksnis <edgars.melnalksnis@gmail.com>
2021-09-30 16:55:31 +0300

    configured GPIOA2 as input. When button pressed, all leds turns on

--- main.c
+++ main.c
@@ -4,12 +4,8 @@
 
 int main(void)
 {
-    uint32_t dummy_del=0;
     uint32_t bcd_time=0;
-    Time time;
-    time.sec=0;
-    time.min=0;
-    time.hours=0;
+    
     Led led_array[21]={{LED_CTRL_3,LED_CTRL_0},
         {LED_CTRL_2,LED_CTRL_1},
         {LED_CTRL_5,LED_CTRL_0},
@@ -43,22 +39,16 @@ int main(void)
     RCC->CSR |= (1<<17);//LSI as internal clock source
     RCC->CSR |= (1 << 18);//enable RTC
 
-    //led_test_array(led_array);
+    GPIOA -> MODER &=~ (1<<5|1<<4);//GPIOA input mode
+    GPIOA -> PUPDR |= (1<<5);//GPIOA2 pulldown
 
     while(1)
     {
-        time.sec=RTC -> TR;
         bcd_time = RTC -> TR;
         bcd_to_display(bcd_time,led_array);
-        //dummy_del++;
-       // if(time.sec & 1 )
-       // {
-        //    blink_led_struct(led_array[0]);
-       // }
+        if((GPIOA->IDR>>2) & 1)
+        {
+            led_test();
+        }
     }
 }
-
-
-
-
-
